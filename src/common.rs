98121d26b6c26019e6c43b47d8cd8d392d29ca2c
matej.fric <matej.fric@storaenso.com>
2024-11-30 22:50:08 +0100

    add helper function bytes_to_i64

--- src/common.rs
+++ src/common.rs
@@ -1,7 +1,17 @@
+use core::str;
+
 use bytes::Bytes;
 
 use crate::err::RedisCommandError;
 
 pub fn bytes_to_string(bytes: &Bytes) -> anyhow::Result<String, RedisCommandError> {
     String::from_utf8(bytes.to_vec()).map_err(|e| RedisCommandError::InvalidUtf8(e.to_string()))
+}
+
+pub fn bytes_to_i64(bytes: &Bytes) -> anyhow::Result<i64, RedisCommandError> {
+    let s = str::from_utf8(bytes)
+        .map_err(|e| RedisCommandError::InvalidUtf8(e.to_string()))?
+        .trim();
+    s.parse::<i64>()
+        .map_err(|e| RedisCommandError::ParseIntError(e.to_string()))
 }
